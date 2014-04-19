79d4535029af83953b62e81689136761d40200b9
Stahnke <thestahnkiest@gmail.com>
2014-04-18 22:53:41 -0700

    Added WaterCollision
    +WaterCollision script
    +WaterCollision object

---
+++ Spring Game Jam/Assets/Scripts/WaterCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterCollision : MonoBehaviour {
+
+	void OnTriggerEnter(Collider player)
+	{
+		if(player.tag == "Player")
+			player.SendMessageUpwards("WaterOn", SendMessageOptions.DontRequireReceiver);
+	}
+	
+	void OnTriggerExit(Collider player)
+	{
+		if(player.tag == "Player")
+			player.SendMessageUpwards("WaterOff", SendMessageOptions.DontRequireReceiver);	
+	}
+}
