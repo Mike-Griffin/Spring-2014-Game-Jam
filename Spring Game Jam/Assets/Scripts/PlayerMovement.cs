b8e386cf268c8bcf90a3f70b7068cf34d3dfeffb
Mike-Griffin <mike_griffin@comcast.net>
2014-04-19 16:17:57 -0700

    Started Water Rising

--- Spring Game Jam/Assets/Scripts/PlayerMovement.cs
+++ Spring Game Jam/Assets/Scripts/PlayerMovement.cs
@@ -65,6 +65,7 @@ public class PlayerMovement : MonoBehaviour {
 
 	void WaterOn()
 	{
+		Debug.Log ("Collided with water");
 		inWater = true;
 		grounded = true;
 	}
---
+++ Spring Game Jam/Assets/Scripts/WaterRising.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterRising : MonoBehaviour {
+	public float KeyDelay = 0.000f;
+	private float height = 0;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(IncreaseBox());
+	}
+	
+	// Update is called once per frame
+
+	public IEnumerator IncreaseBox()
+	{
+		while(true)
+		{
+			height += 10;
+			yield return new WaitForSeconds(KeyDelay);
+		}
+	}
+
+	void Update () {
+	
+	}
+}
