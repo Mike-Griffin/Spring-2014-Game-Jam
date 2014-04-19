b2c0e86f046b93ef2ff951c8d627be472fd5c5fc
Mike-Griffin <mike_griffin@comcast.net>
2014-04-18 23:00:27 -0700

    Started Movement, jumping
    Jumping is working with some other assets

---
+++ Spring Game Jam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	bool grounded = true;
+	bool inWater = false;
+	public float LandMovementSpeed = 10;
+	public float jumpForce = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		/*
+		if(Input.GetAxis("Horizontal")
+		{
+
+		}
+*/
+		if (!inWater && grounded && Input.GetKeyDown (KeyCode.Space)) 
+		{
+			Jump();
+			Debug.Log("On the ground");
+		}
+	}
+
+	void WaterOn()
+	{
+		inWater = true;
+	}
+
+	void WaterOff()
+	{
+		inWater = false;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Platform") 
+		{
+			grounded = true;
+		}
+	}
+
+	void Jump()
+	{
+		rigidbody2D.AddForce (Vector2.up * jumpForce);
+		grounded = false;
+	}
+}
