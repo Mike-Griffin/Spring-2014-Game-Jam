49f4279588110775945fc0a7c800543549e4bfce
Stahnke <thestahnkiest@gmail.com>
2014-04-20 01:29:37 -0700

    Fixed Crystalizing
    Still need to make it not collide with bombs and other objects though

---
+++ Spring Game Jam/Assets/Scripts/CrystalSpawn.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrystalSpawn : MonoBehaviour {
+
+	public GameObject SpawnPointL;
+	public GameObject SpawnPointR;
+	public GameObject SpawnPointU;
+	public GameObject SpawnPointD;
+
+	public bool LeftFree, UpFree, RightFree, DownFree;
+
+	public float SpawnDelay = 0.5f;
+	public float MiniDelay = 0.124f;
+	public GameObject Crystal;
+
+	public void Start(){
+
+		LeftFree = UpFree = RightFree = DownFree = true;
+		StartCoroutine(SpawnCrystals());
+	}
+
+	public void OnTriggerStay2D(Collider2D collider)
+	{
+		if(collider.tag != "Player" && collider.tag != "Water" && collider.tag != "Spawn"
+		   && collider.tag != "SpawnLeft" && collider.tag != "SpawnRight")
+		{
+			Destroy (gameObject);
+		}
+	}
+	
+	// Use this for initialization
+	public IEnumerator SpawnCrystals(){
+
+		yield return new WaitForSeconds(SpawnDelay);
+
+		if(LeftFree)
+		{
+			Instantiate(Crystal, SpawnPointL.transform.position, SpawnPointL.transform.rotation);
+			//SpawnPointL.SetActive (false);
+			yield return new WaitForSeconds(MiniDelay);
+		}
+		if(UpFree)
+		{
+			Instantiate(Crystal, SpawnPointU.transform.position, SpawnPointU.transform.rotation);
+			//SpawnPointU.SetActive (false);
+			yield return new WaitForSeconds(MiniDelay);
+		}
+
+		if(RightFree)
+		{
+			Instantiate(Crystal, SpawnPointR.transform.position, SpawnPointR.transform.rotation);
+			//SpawnPointR.SetActive (false);
+			yield return new WaitForSeconds(MiniDelay);
+		}
+		if(DownFree)
+		{
+			Instantiate(Crystal, SpawnPointD.transform.position, SpawnPointD.transform.rotation);
+			//SpawnPointD.SetActive (false);
+		}
+	}
+	//SDirection values:
+	//1 = L, 2 = U, 3 = R, 4 = D;
+	public void CheckNotFree(int SDirection){
+		if(SDirection == 1)
+		{
+			LeftFree = false;
+		}
+		if(SDirection == 2)
+		{
+			UpFree = false;
+		}
+		if(SDirection == 3)
+		{
+			RightFree = false;
+		}
+		if(SDirection == 4)
+		{
+			DownFree = false;
+		}
+
+	}
+	
+}
---
+++ Spring Game Jam/Assets/Scripts/CrystalSpawnPoints.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrystalSpawnPoints : MonoBehaviour {
+	
+	public bool L, U, R, D;
+	public bool Checked = true;
+	// Use this for initialization
+
+	void OnTriggerStay2D(Collider2D collider)
+	{
+		if(collider.tag != "Player" && collider.tag != "Water" && Checked == true)
+		{
+			if(L)
+			{
+				Debug.Log ("STOP SPAWNING!!!");
+				transform.parent.SendMessageUpwards("CheckNotFree", 1, 
+			                                  	SendMessageOptions.DontRequireReceiver);
+			}
+			else if(U && collider.tag != "SpawnLeft" && collider.tag!= "SpawnRight")
+			{
+				Debug.Log ("STOP SPAWNING!!!");
+				transform.parent.SendMessageUpwards("CheckNotFree", 2, 
+				                                    SendMessageOptions.DontRequireReceiver);
+			}
+			else if(R)
+			{
+				Debug.Log ("STOP SPAWNING!!!");
+				transform.parent.SendMessageUpwards("CheckNotFree", 3, 
+				                                    SendMessageOptions.DontRequireReceiver);
+			}
+			else if(D && collider.tag != "SpawnLeft" && collider.tag!= "SpawnRight")
+			{
+				Debug.Log ("STOP SPAWNING!!!");
+				transform.parent.SendMessageUpwards("CheckNotFree", 4, 
+				                                    SendMessageOptions.DontRequireReceiver);
+
+			}
+			Checked = false;
+		}
+	}
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if(collider.tag != "Player" && collider.tag != "Water")
+			{
+				if(L)
+				{
+					Debug.Log ("STOP SPAWNING!!!");
+					transform.parent.SendMessageUpwards("CheckNotFree", 1, 
+					                                    SendMessageOptions.DontRequireReceiver);
+				}
+				else if(U && collider.tag != "SpawnLeft" && collider.tag!= "SpawnRight")
+				{
+					Debug.Log ("STOP SPAWNING!!!");
+					transform.parent.SendMessageUpwards("CheckNotFree", 2, 
+					                                    SendMessageOptions.DontRequireReceiver);
+				}
+				else if(R)
+				{
+					Debug.Log ("STOP SPAWNING!!!");
+					transform.parent.SendMessageUpwards("CheckNotFree", 3, 
+					                                    SendMessageOptions.DontRequireReceiver);
+				}
+				else if(D && collider.tag != "SpawnLeft" && collider.tag!= "SpawnRight")
+				{
+					Debug.Log ("STOP SPAWNING!!!");
+					transform.parent.SendMessageUpwards("CheckNotFree", 4, 
+					                                    SendMessageOptions.DontRequireReceiver);
+					
+				}
+			}
+		}
+}
